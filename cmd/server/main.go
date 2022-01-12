b7869222e2d97d6f0d20007bedda0c4a27044974
sojoudian <sojodyan@gmail.com>
2022-01-11 23:35:20 -0500

    update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,16 @@
 package main
 
+import "log"
+
+func Run() error {
+	//Responsible for initializing and starting
+	//The gRPC server
+	return nil
+}
+
 func main() {
+	if err := R; err != nil {
+		log.Fatal(err)
+	}
 
 }
